e1565c757bdf09dbd65098abef7aa1fe5d0d69de
Kim <36648257+kim-ras@users.noreply.github.com>
2021-11-10 18:18:18 +0100

    Update test-100-times.sh

--- test-100-times.sh
+++ test-100-times.sh
@@ -1,7 +1,7 @@
 i=0
 while [ $i -ne 10 ]
 do
-    curl -s -o /dev/null -I -w "%{response_code}-%{stdout}" https://yara-app.azurewebsites.net/
+    curl -s -o /dev/null -I -w "%{response_code}-%{stdout}" https://xxxxx.azurewebsites.net/
 
 
     i=$(($i+1))
